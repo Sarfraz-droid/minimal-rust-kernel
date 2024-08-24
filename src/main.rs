0a68e080d214f4a66c9202e9d5a8da6b8458057f
Sarfraz Alam <sarfrazalam@Sarfrazs-Laptop.local>
2024-08-24 23:32:41 +0530

    Init
    init rust minimal kernel

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+static HELLO:&[u8] = b"Hello World!";
+
+/// This function is called on panic.
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+
+    println!("Hello World{}", "!");
+
+    loop {}
+}
